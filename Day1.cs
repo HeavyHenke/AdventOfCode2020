5a45fde24c8da53ce8d3ba869f43149040159b56
Henrik Hellenborg <henrik@projektrsf.se>
2020-12-01 10:38:05 +0100

    Day1

---
+++ Day1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode2020
+{
+    internal class Day1
+    {
+        public string CalcA()
+        {
+            var values = File.ReadAllLines("Day01.txt")
+                .Select(int.Parse)
+                .ToHashSet();
+
+            foreach(var val in values)
+            {
+                var toSeek = 2020 - val;
+                if (values.Contains(toSeek))
+                    return (val * toSeek).ToString();
+            }
+
+            throw new Exception("Nothing found!");
+        }
+
+        public string CalcB()
+        {
+            var values = File.ReadAllLines("Day01.txt")
+                .Select(int.Parse)
+                .ToList();
+            var valuesHash = values.ToHashSet();
+
+            for(int i = 0; i < values.Count; i++)
+            {
+                var left = 2020 - values[i];
+                for(int j = i+1; j < values.Count; j++)
+                {
+                    var toSeek = left - values[j];
+                    if (valuesHash.Contains(toSeek))
+                        return (values[i] * values[j] * toSeek).ToString();
+                }
+            }
+
+            foreach (var val in values)
+            {
+                var toSeek = 2020 - val;
+                if (values.Contains(toSeek))
+                    return (val * toSeek).ToString();
+            }
+
+            throw new Exception("Nothing found!");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2020
+{
+    class Program
+    {
+        static void Main()
+        {
+            DateTime start = DateTime.Now;
+            string result = new Day1().CalcB();
+            DateTime stop = DateTime.Now;
+
+            Console.WriteLine("It took " + (stop - start).TotalSeconds);
+
+            WindowsClipboard.SetText(result);
+            Console.WriteLine(result);
+        }
+    }
+}
