9a8fe2d332180b480c8459ea1a91017496fc7a7e
Henrik Hellenborg <henrik@projektrsf.se>
2020-12-06 20:19:19 +0100

    Day6

---
+++ Day6.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace AdventOfCode2020
+{
+    internal class Day6
+    {
+        public int CalcA()
+        {
+            var lines = File.ReadAllLines("Day06.txt");
+            int sum = 0;
+            var hash = new HashSet<char>();
+
+            foreach (var line in lines)
+            {
+                if (line == "")
+                {
+                    sum += hash.Count;
+                    hash.Clear();
+                }
+
+                foreach (var s in line)
+                    hash.Add(s);
+            }
+
+            sum += hash.Count;
+
+            return sum;
+        }
+
+        public int CalcB()
+        {
+            var lines = File.ReadAllLines("Day06.txt");
+            int sum = 0;
+            var groupHash = new HashSet<char>();
+
+            bool first = true;
+            foreach (var line in lines)
+            {
+                if (line == "")
+                {
+                    sum += groupHash.Count;
+                    groupHash.Clear();
+                    first = true;
+                    continue;
+                }
+
+                if (first)
+                {
+                    foreach (var s in line)
+                        groupHash.Add(s);
+                    first = false;
+                }
+                else
+                    groupHash.IntersectWith(line);
+            }
+
+            sum += groupHash.Count;
+
+            return sum;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -13,7 +13,7 @@ namespace AdventOfCode2020
         static void Main()
         {
             DateTime start = DateTime.Now;
-            string result = new Day5().CalcB().ToString();
+            string result = new Day7().CalcA().ToString();
             DateTime stop = DateTime.Now;
 
             Console.WriteLine("It took " + (stop - start).TotalSeconds);
@@ -21,5 +21,19 @@ namespace AdventOfCode2020
             WindowsClipboard.SetText(result);
             Console.WriteLine(result);
         }
+    }
+
+    internal class Day7
+    {
+        public string CalcA()
+        {
+            return "not impl.";
+        }
+
+        public string CalcB()
+        {
+            return "not impl.";
+        }
+
     }
 }
