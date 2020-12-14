90323f286fdd9770825e2aef7e120c0e6904318c
Henrik Hellenborg <henrik@projektrsf.se>
2020-12-14 20:06:22 +0100

    Day14

---
+++ Day14.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode2020
+{
+    internal class Day14
+    {
+        public long CalcA()
+        {
+            var input = File.ReadAllLines("Day14.txt");
+
+            long orMask = 0;
+            long andMask = 0xFFFF_FFFF;
+
+            var regEx = new Regex(@"mem\[(\d+)] = (\d+)", RegexOptions.Compiled);
+            var memory = new Dictionary<long, long>();
+            foreach (var line in input)
+            {
+                if (line.StartsWith("mask"))
+                {
+                    var maskPart = line.Split(" = ")[1];
+                    orMask = Convert.ToInt64(maskPart.Replace('X','0'), 2);
+                    andMask = Convert.ToInt64(maskPart.Replace('X', '1'), 2);
+                    continue;
+                }
+
+                var m = regEx.Match(line);
+                var addr = long.Parse(m.Groups[1].Value);
+                var val = long.Parse(m.Groups[2].Value);
+
+                val &= andMask;
+                val |= orMask;
+                memory[addr] = val;
+            }
+
+            return memory.Values.Sum();
+        }
+
+        public long CalcB()
+        {
+            var input = File.ReadAllLines("Day14.txt");
+
+            long orMask = 0;
+            long floatingMask = 0;
+
+            var regEx = new Regex(@"mem\[(\d+)] = (\d+)", RegexOptions.Compiled);
+            var memory = new Dictionary<long, long>();
+            foreach (var line in input)
+            {
+                if (line.StartsWith("mask"))
+                {
+                    var maskPart = line.Split(" = ")[1];
+                    orMask = Convert.ToInt64(maskPart.Replace('X', '0'), 2);
+                    floatingMask = Convert.ToInt64(maskPart.Replace('1', '0').Replace('X', '1'), 2);
+                    continue;
+                }
+
+                var m = regEx.Match(line);
+                var addr = long.Parse(m.Groups[1].Value);
+                var val = long.Parse(m.Groups[2].Value);
+
+                addr |= orMask;
+
+                foreach(var realAddr in GetAllAddresses(addr, floatingMask))
+                    memory[realAddr] = val;
+            }
+
+            return memory.Values.Sum();
+        }
+
+        private IEnumerable<long> GetAllAddresses(long addr, long floatMask)
+        {
+            if (floatMask == 0)
+            {
+                yield return addr;
+                yield break;
+            }
+
+            var ix = IndexOfFirstSetBit(floatMask);
+            var floatMaskMinusOneBit = floatMask & (0xFFFF_FFFF_FFFF ^ (1L << ix));
+
+            addr &= (0xFFFF_FFFF_FFFF ^ (1L << ix));
+            foreach (var sub in GetAllAddresses(addr, floatMaskMinusOneBit))
+                yield return sub;
+
+            addr |= (1L << ix);
+            foreach (var sub in GetAllAddresses(addr, floatMaskMinusOneBit))
+                yield return sub;
+        }
+
+        private int IndexOfFirstSetBit(long test)
+        {
+            var binForm = Convert.ToString(test, 2);
+            return binForm.Length - binForm.IndexOf('1') - 1;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections;
 using System.Diagnostics;
+using System.IO;
+using System.Linq;
 using System.Linq.Expressions;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
+using MoreLinq;
 
 namespace AdventOfCode2020
 {
@@ -26,17 +29,22 @@ namespace AdventOfCode2020
         }
     }
 
+
     internal class Day13
     {
         public int CalcA()
         {
-            return -1;
-        }
+            var input = File.ReadAllLines("Day13.txt");
+            var earliestDeparture = int.Parse(input[0]);
+            var (id, waitTime) = input[1]
+                .Split(",")
+                .Where(s => s != "x")
+                .Select(int.Parse)
+                .Select(id => (id, waitTime: (1 + earliestDeparture / id) * id - earliestDeparture))
+                .MinBy(tuple => tuple.waitTime)
+                .First();
 
-        public int CalcB()
-        {
-            return -1;
+            return waitTime * id;
         }
-
     }
 }
