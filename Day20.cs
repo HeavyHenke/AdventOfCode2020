2e9263c3d0bb208d93fc853f9594b78da888d1ec
Henrik Hellenborg <henrik@projektrsf.se>
2020-12-29 12:35:33 +0100

    Day24

---
+++ Day20.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode2020
+{
+    class Day20
+    {
+        public long CalcA()
+        {
+            var totalSides = new Dictionary<string, int>();
+            var tiles = new Dictionary<int, List<string>>();
+
+            var lines = File.ReadAllLines("Day20.txt");
+            for (int i = 0; i < lines.Length; i++)
+            {
+                int tileId = int.Parse(lines[i].Substring(5, lines[i].Length - 5 - 1));
+                var left = new char[10];
+                var right = new char[10];
+                i++;
+                var firstRow = lines[i];
+                for (int row = 0; row < 10; row++)
+                {
+                    left[row] = lines[i][0];
+                    right[row] = lines[i][9];
+                    i++;
+                }
+
+                var sides = new List<string>
+                {
+                    new string(left), new string(right), firstRow, lines[i-1]
+                };
+
+                tiles.Add(tileId, sides);
+                foreach (var side in sides)
+                {
+                    if (totalSides.ContainsKey(side))
+                        totalSides[side]++;
+                    else
+                        totalSides.Add(side, 1);
+                    var side2 = new string(side.Reverse().ToArray());
+                    if (totalSides.ContainsKey(side2))
+                        totalSides[side2]++;
+                    else
+                        totalSides.Add(side2, 1);
+                }
+            }
+
+            var product = tiles.Select(t => (t.Key, numNonMatchingSides: t.Value.Count(v => totalSides[v] == 1)))
+                .Where(t => t.numNonMatchingSides == 2)
+                .Select(t => t.Key)
+                .Aggregate(1L, (current, tileId) => current * tileId);
+
+            return product;
+        }
+
+        public long CalcB()
+        {
+            var totalSides = new Dictionary<string, List<int>>();
+            var tiles = new Dictionary<int, List<string>>();
+
+            var lines = File.ReadAllLines("Day20.txt");
+            for (int i = 0; i < lines.Length; i++)
+            {
+                int tileId = int.Parse(lines[i].Substring(5, lines[i].Length - 5 - 1));
+                var left = new char[10];
+                var right = new char[10];
+                i++;
+                var firstRow = lines[i];
+                for (int row = 0; row < 10; row++)
+                {
+                    left[row] = lines[i][0];
+                    right[row] = lines[i][9];
+                    i++;
+                }
+
+                var sides = new List<string>
+                {
+                    new string(left), new string(right), firstRow, lines[i-1]
+                };
+
+                tiles.Add(tileId, sides);
+                foreach (var side in sides)
+                {
+                    if (totalSides.ContainsKey(side))
+                        totalSides[side].Add(tileId);
+                    else
+                        totalSides.Add(side, new List<int> { tileId });
+                    var side2 = new string(side.Reverse().ToArray());
+                    if (totalSides.ContainsKey(side2))
+                        totalSides[side2].Add(tileId);
+                    else
+                        totalSides.Add(side2, new List<int> { tileId });
+                }
+            }
+
+            return -1;
+        }
+    }
+}
---
+++ Day24.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode2020
+{
+    internal class Day24
+    {
+        public int CalcA()
+        {
+            var blackTiles = new HashSet<(int q, int r)>();    // Axial coordinate system
+
+            foreach (var line in File.ReadAllLines("Day24.txt"))
+            {
+                (int q, int r) pos = (0, 0);
+
+                for (int i = 0; i < line.Length; i++)
+                {
+                    if (line[i] == 'e')
+                    {
+                        pos.q--;
+                    }
+                    else if (line[i] == 'w')
+                    {
+                        pos.q++;
+                    }
+                    else if (line[i] == 's')
+                    {
+                        pos.r++;
+                        if (line[i + 1] == 'e') 
+                            pos.q--;
+                        i++;
+                    }
+                    else if (line[i] == 'n')
+                    {
+                        pos.r--;
+                        if (line[i + 1] == 'w')
+                            pos.q++;
+                        i++;
+                    }
+                    else
+                        throw new Exception("Knas");
+                }
+
+                if (blackTiles.Add(pos) == false)
+                    blackTiles.Remove(pos);
+            }
+
+            return blackTiles.Count;
+        }
+
+        public int CalcB()
+        {
+            var blackTiles = new HashSet<(int q, int r)>();    // Axial coordinate system
+
+            foreach (var line in File.ReadAllLines("Day24.txt"))
+            {
+                (int q, int r) pos = (0, 0);
+
+                for (int i = 0; i < line.Length; i++)
+                {
+                    if (line[i] == 'e')
+                    {
+                        pos.q--;
+                    }
+                    else if (line[i] == 'w')
+                    {
+                        pos.q++;
+                    }
+                    else if (line[i] == 's')
+                    {
+                        pos.r++;
+                        if (line[i + 1] == 'e')
+                            pos.q--;
+                        i++;
+                    }
+                    else if (line[i] == 'n')
+                    {
+                        pos.r--;
+                        if (line[i + 1] == 'w')
+                            pos.q++;
+                        i++;
+                    }
+                    else
+                        throw new Exception("Knas");
+                }
+
+                if (blackTiles.Add(pos) == false)
+                    blackTiles.Remove(pos);
+            }
+
+            for (int day = 0; day < 100; day++)
+            {
+                var nextDay = new HashSet<(int q, int r)>();
+                foreach (var blackTile in blackTiles)
+                {
+                    var neighbors = GetAdjacent(blackTile).Count(t => blackTiles.Contains(t));
+                    if (neighbors == 1 || neighbors == 2)
+                        nextDay.Add(blackTile);
+                }
+
+                var whitesToVisit = blackTiles.SelectMany(GetAdjacent).Where(t => blackTiles.Contains(t) == false).Distinct();
+                foreach (var tile in whitesToVisit)
+                {
+                    var neighbors = GetAdjacent(tile).Count(t => blackTiles.Contains(t));
+                    if (neighbors == 2)
+                        nextDay.Add(tile);
+                }
+
+                blackTiles = nextDay;
+            }
+
+            return blackTiles.Count;
+        }
+
+        private static IEnumerable<(int q, int r)> GetAdjacent((int q, int r) pos)
+        {
+            yield return (pos.q - 1, pos.r);
+            yield return (pos.q + 1, pos.r);
+
+            yield return (pos.q - 1, pos.r + 1);
+            yield return (pos.q + 1, pos.r - 1);
+
+            yield return (pos.q, pos.r + 1);
+            yield return (pos.q, pos.r - 1);
+        }
+    }
+}
